fb0cec974bf48e7a2488f21d9f1b4073bc1ed175
Felipe Castro <felpokun123@gmail.com>
2023-01-10 18:13:14 -0300

    ft: ¨Integracion middleware express-rate-limit¨
    Intregracion middleware para el endpoint de login para evitar ataques de Dos y fuerza bruta

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,18 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { login } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares');
+const ratelimit = require("express-rate-limit");
 
 const router = Router();
-
+const limitaPeticiones = ratelimit({
+    windowMs: 10 * 60 * 1000, // 10 minutos 
+    max: 15,              // 15 intentos dentro de los 10 minutos
+    message: { msg: 'Demasiados intentos, por favor intente mas tarde'},
+    standardHeaders: true,
+    legacyHeaders: false,
+    skipSuccessfulRequests: true
+})
 router.post('/login',
-    [check('run', 'El rut es obligatorio.').not().notEmpty(), validarCampos], login);
+    [check('run', 'El rut es obligatorio.').not().notEmpty(), validarCampos, limitaPeticiones], login);
 
 module.exports = router;
